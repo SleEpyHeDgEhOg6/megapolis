285956927bce5053a09bd6d8efa5e7a61ada389e
Vlada <vlada20066v@gmail.com>
2024-02-14 16:35:01 +0300

    task3

---
+++ task3.py
@@ -0,0 +1,15 @@
+import csv
+
+with open('/home/user/Загрузки/vacancy.csv', encoding='utf-8') as file:
+    reader = csv.reader(file, delimiter=';')
+    reader1 = list(reader)
+
+company = input()
+
+while company != 'None':
+    for el in reader1:
+        if el[-1]==company:
+            print(f'В {company} найдена вакансия: {el[-2]}. З/п составит:{el[-5]}')
+    else:
+        print('К сожалению, ничего не удалось найти.')
+    company = input()
